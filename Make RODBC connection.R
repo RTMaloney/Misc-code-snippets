da4d94f07be90d2e2ae49699c6e6e206fc629f4f
Ryan T. Maloney <maloneyrt@gmail.com>
2021-06-28 11:41:55 +1000

    Update Make RODBC connection.R
    Included details of how to find list of installed ODBC drivers on Windows machine.

--- Make RODBC connection.R
+++ Make RODBC connection.R
@@ -1,4 +1,8 @@
 
+# Note that different ODBC drivers will be installed on different Windows devices.
+# To see what drivers are installed, go to Control Panel > Administrative Tools > ODBC Data Sources (64-bit).
+# Click on the 'Drivers' tab to see the installed drivers listed.
+
 library(RODBC)
 
 # Set up direct connection to SQL server database engine, using Windows credentials
