d6a3eee8d21042aa55456af27deeac6ed0a0cdd0
Ryan T. Maloney <maloneyrt@gmail.com>
2021-08-22 23:48:45 +1000

    Create R specify file directory based on computer identity.R
    Identify the computer nodename and specify the file path depending on the PC you're using.
    Not needed if using the 'here' package within an R project.

---
+++ R specify file directory based on computer identity.R
@@ -0,0 +1,6 @@
+comp_dets <- Sys.info() # Find out computer identity
+if (comp_dets['nodename'] == 'workPC'){
+  x <- ' C:/Users/ryan.maloney/Documents/.../filename.extension'
+else
+  x <- 'C:/Users/RyanDataPC/Documents/.../filename.extension'
+}
