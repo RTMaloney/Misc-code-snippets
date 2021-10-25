cc13a2296f84bb86c86af35e9daf0eba94e325d9
Ryan T. Maloney <maloneyrt@gmail.com>
2021-10-25 17:02:12 +1100

    Update R make summed NAs equal NA.R
    Minor update: added line to give logical array where ALL entries in a given row ==NA; as opposed to any where sporadic NAs might be embedded amongst valid values.

--- R make summed NAs equal NA.R
+++ R make summed NAs equal NA.R
@@ -5,4 +5,6 @@
 
 # See here for inspiration: https://stackoverflow.com/questions/18708647/logical-vector-with-multiple-columns-in-r
 
-valid_sum <- apply(df1, 1, function(x) any(!is.na(x)) )
+valid_sum   <- apply(df1, 1, function(x) any(!is.na(x)) ) # Provides a flag where at least 1 entry is NOT null
+                   
+invalid_sum <- apply(df1, 1, function(x) all(is.na(x)) ) # provides a flag where ALL entries in the row == NA
