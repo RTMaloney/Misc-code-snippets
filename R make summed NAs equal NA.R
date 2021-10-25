ebcfdef02370fe00a77be3b75ebd37ff03cb4b41
Ryan T. Maloney <maloneyrt@gmail.com>
2021-10-25 16:46:19 +1100

    Create R make summed NAs equal NA.R

---
+++ R make summed NAs equal NA.R
@@ -0,0 +1,8 @@
+# If you are summing across rows, if all entries in a row = NA, then the result = 0.
+# I don't know why the sum of NAs does not == NA.
+
+# This operation allows you to sum rows accounting for the case where all entries == NA, but makes the result in this case == NA and != 0.
+
+# See here for inspiration: https://stackoverflow.com/questions/18708647/logical-vector-with-multiple-columns-in-r
+
+valid_sum <- apply(df1, 1, function(x) any(!is.na(x)) )
